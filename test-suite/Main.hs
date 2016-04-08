45a95cba3eecabdb538b641bc05b8608005c2a53
Taylor Fausak <taylor@fausak.me>
2016-04-08 07:40:07 -0500

    Add helper function for generating test expectations

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 
+import Data.Monoid ((<>))
 import qualified Data.Proxy as Proxy
+import qualified Data.Text as Text
 import qualified Lackey
 import qualified Servant.API as Servant
 import qualified Test.Tasty as Tasty
@@ -18,4 +20,19 @@ spec = parallel $ do
     describe "rubyForAPI" $ do
         it "supports get requests" $ do
             let api = Proxy.Proxy :: Proxy.Proxy (Servant.Get '[Servant.JSON] ())
-            Lackey.rubyForAPI api `shouldBe` "def get(excon)excon.request(:method=>:get,:path=>\"/\",:headers=>{},:body=>nil)end"
+            Lackey.rubyForAPI api `shouldBe`
+                ruby "get" "" "get" "" "" False
+
+-- Since every generated function has the same structure, it can be abstracted
+-- away behind this function.
+ruby :: Text.Text -> Text.Text -> Text.Text -> Text.Text -> Text.Text -> Bool -> Text.Text
+ruby name params method path headers body = "\
+    \def " <> name <> "(excon" <> params <> ")\
+      \excon.request(\
+        \:method=>:" <> method <> ",\
+        \:path=>\"/" <> path <> "\",\
+        \:headers=>{" <> headers <> "},\
+        \:body=>" <> if body then "body" else "nil" <> "\
+      \)\
+    \end\
+\"
